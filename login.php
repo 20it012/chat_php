14746f29ade0fef8a6ed946cbb57c56a7b0f1729
divyesh <20it012@charusat.edu.in>
2022-07-30 09:35:36 +0530

    initial commit

---
+++ login.php
@@ -0,0 +1,40 @@
+<?php 
+    session_start();
+    if(isset($_SESSION['unique_id'])){ //if user is logged in
+        header("location: user.php");
+    }
+?>
+<?php include_once "header.php"; ?>
+<body>
+    <!--header section start -->
+    <div class="wrapper">
+        <section class="form login">
+            <header>Chat App</header>
+            <form action="#">
+                <div class="error-text">               
+                </div>
+                    
+                <div class="field input">
+                    <label>Email Address</label>
+                    <input type="text" name="email" placeholder="Enter your email">
+                </div>
+                <div class="field input">
+                    <label>Password</label>
+                    <input type="password" name="password" placeholder="Enter your Password">
+                    <i class="fas fa-eye"></i>
+                </div>
+                
+                <div class="field button">
+                    <input type="submit" value="Continue to Chat">
+                </div>
+            </form>
+            <div class="link">Not yet signed up? <a href="index.php">signup  now</a></div>
+        </section>
+    </div>
+    <!--header section end -->
+
+<script src="javascript/pass-show-hide.js"></script>
+<script src="javascript/login.js"></script>
+
+</body>
+</html>
